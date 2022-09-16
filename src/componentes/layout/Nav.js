cebbe9b720db6e006dba1ea67c0b40937dccca3d
Gonzalo Cepeda <gjcepeda9@gmail.com>
2022-09-16 18:32:12 -0300

    Nav Terminado

--- src/componentes/layout/Nav.js
+++ src/componentes/layout/Nav.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState,useEffect} from "react";
 import { NavLink } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
@@ -7,22 +7,40 @@ import "../../estilos/componentes/layout/Nav.css";
 
 function Nav () {
   
+  function ShowWindowWidth() {
+  const [width, setWidth] = useState(0);
+
+  useEffect(() => {
+    // Creamos una función para actualizar el estado con el clientWidth
+    const updateWidth = () => {
+      const width = document.body.clientWidth
+      setWidth(width)
+    }
+    // Actualizaremos el width al montar el componente
+    updateWidth()
+    // Nos suscribimos al evento resize() de window
+    window.addEventListener("resize", updateWidth)
+  })
+  return width;
+}
+
   const [clicked, setClicked] = useState(false);
 
-  function handleClick () {
-    console.log(clicked);
+  function mostrarMenu () {
     setClicked(!clicked);
   }
 
+  var width = ShowWindowWidth() 
+
   return (
     <nav className="contenedor-nav">
-      <ul className={clicked ? "nav-lista--show" : "nav-lista"}>
+      <ul className={clicked && width <= 480 ? "nav-lista--show" : "nav-lista"} id="navResponsive">
         <li className="lista-item"><NavLink to="/">Inicio</NavLink></li>
         <li className="lista-item"><NavLink to="/plantel">Plantel</NavLink></li>
         <li className="lista-item"><NavLink to="/novedades">Novedades</NavLink></li>
         <li className="lista-item"><NavLink to="/contacto">Contacto</NavLink></li>
       </ul>
-      <button className= 'menu-boton' id="menu-boton" onClick={handleClick}>
+      <button className= 'menu-boton' id="menu-boton" onClick={mostrarMenu}>
         <FontAwesomeIcon icon={clicked ? faXmark : faBars} className="menu-icon" />
       </button>
     </nav>
